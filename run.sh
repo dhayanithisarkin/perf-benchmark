59468afad420cce083b96bc65901a1c91a853b32
Pratik Bhangale <pbhangale@vmware.com>
2019-08-26 14:42:18 +0530

    User input in run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,31 @@ if [ ! -d ./tmp ]
 then
     mkdir -p ./tmp
 fi
+cmd="python ./benchmark/driver.py"
 
-python ./benchmark/driver.py -did DPSZ9NG -w 1 -cw 0 -bw 1
+read -p "Enter DID (Default[Home Depot] = DP10XVX) : " DID
+if ! test -z "$DID"
+then
+      cmd="$cmd -did $DID"
+fi
+read -p "Enter Window (Default 1 day): " w
+if ! test -z "$w"
+then
+      cmd="$cmd -w $w"
+fi
+read -p "Enter Current Time (Default 0): " cw
+if ! test -z "$cw"
+then
+      cmd="$cmd -cw $cw"
+fi
+read -p "Enter Base Time (Default 1): " bw
+if ! test -z "$bw"
+then
+      cmd="$cmd -bw $bw"
+fi
 
+eval $cmd
 pdflatex ./benchmark/tex_template.tex
+
 rm tex_template.aux
 rm tex_template.log
